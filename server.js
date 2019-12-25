021f93108fd625aa8aad4cdc0e9b459eae90d19f
anurag <anurag.sandhu@springrole.com>
2019-12-25 12:21:42 +0530

    Adding a logging framework

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
 const express = require('express'),
-    path = require('path'),
-    config = require('./config'),
-    // routes = require('./routes'),
-    router = express.Router(),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    port = config.server.PORT,
-taskRepo = require('./dbLayer/repositories/taskRepo'),
-taskService = require('./server/task/taskService');
-
+  path = require('path'),
+  config = require('./config'),
+  // routes = require('./routes'),
+  router = express.Router(),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  port = config.server.PORT,
+  taskRepo = require('./dbLayer/repositories/taskRepo'),
+  taskService = require('./server/task/taskService'),
+  winston = require('./config/winston'),
+  morgan = require('morgan');
+ 
 const MongoClient = require("mongodb").MongoClient;
 const crypto = require("crypto");
 const uri = "mongodb://localhost:27017/";
 
 // Export app for other routes to use
 let app = express(),
-    http = require('http').Server(app);
+  http = require('http').Server(app);
 
 // Starting point of the server
 function main() {
   app.use(express.static('./public'));
-
+  
   app.use(express.static(path.join(__dirname + '/public')));
   app.use(bodyParser.urlencoded({ // Middleware
     extended: true
   }));
   app.use(bodyParser.json());
-  app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+  app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
   app.use(methodOverride());
+  app.use(morgan('combined', { stream: winston.stream }));
 
 
   try {
@@ -50,7 +53,7 @@ function main() {
       });
 
       app.use(function (req, res) {
-        res.status(404).send({url: req.originalUrl + ' not found'})
+        res.status(404).send({ url: req.originalUrl + ' not found' })
       });
 
     });
